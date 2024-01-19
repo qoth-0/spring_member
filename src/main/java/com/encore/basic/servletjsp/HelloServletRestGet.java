8cc24ca362982eea53b7aaf9b7f5c9d76da068a6
qoth-0 <bny1324@naver.com>
2024-01-19 14:40:41 +0900

    WebServlet + RestAPI

--- src/main/java/com/encore/basic/servletjsp/HelloServletRestGet.java
+++ src/main/java/com/encore/basic/servletjsp/HelloServletRestGet.java
@@ -1,4 +1,30 @@
 package com.encore.basic.servletjsp;
 
-public class HelloServletRestGet {
+import com.encore.basic.domain.Hello;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello-servlet-rest-get")
+public class HelloServletRestGet extends HttpServlet {
+//    Hello객체 조립 -> res - Contente-type: json, out.print(hello)
+//    out.print()는 String타입을 받으므로 hello객체를 json(stirng)으로 직렬화해서 넣어줘야 함 - ObjectMapper의 writeValueAsString() 사용
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Hello hello = new Hello();
+        hello.setName("qoth");
+        hello.setEmail("qoth@a.com");
+        hello.setPw("1234");
+        ObjectMapper objectMapper = new ObjectMapper();
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        PrintWriter out = res.getWriter();
+        out.print(objectMapper.writeValueAsString(hello));
+        out.flush();
+    }
 }
--- src/main/java/com/encore/basic/servletjsp/HelloServletRestPost.java
+++ src/main/java/com/encore/basic/servletjsp/HelloServletRestPost.java
@@ -1,4 +1,32 @@
 package com.encore.basic.servletjsp;
 
-public class HelloServletRestPost {
+import com.encore.basic.domain.Hello;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello-servlet-rest-post")
+public class HelloServletRestPost extends HttpServlet {
+//    json으로 데이터 주기 json으로 넘어온 데이터를 역직렬화(ObjectMapper-readValue)로 hello저장 콘솔에 띄우기, respose ok
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+//        Json(String)을 객체형태로 변환
+        Hello hello = objectMapper.readValue(req.getReader(), Hello.class);
+        System.out.println(hello);
+        //        응답(header)
+        res.setContentType("text/plain");
+        res.setCharacterEncoding("UTF-8");
+//        응답(body)
+        PrintWriter out = res.getWriter();
+        out.print("ok");
+//        버퍼를 통해 조립이 이루어지므로, 버퍼를 비우는 과정
+        out.flush();
+    }
 }
